6745172f7dea70e26f726ed301b2c83903520d0a
Kayumars <kayumars.nekruz@gmail.com>
2017-11-16 17:40:37 +0100

    ajout mvc publications et acces a publications sur admin

---
+++ administrator/components/com_arvie/views/publications/tmpl/default_headcolumns.php
@@ -0,0 +1,37 @@
+<?php
+defined('_JEXEC') or die('Restricted Access');
+
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+?>
+
+<tr>
+	<th width="1%" class="hidden-phone">
+		<?php echo JHtml::_('grid.checkall'); ?> 
+	</th>
+	<th width="2%">
+		<?php echo JHtml::_('grid.sort', 'COM_ARVIE_PUBLICATIONS_ID', 'p.id', $listDirn, $listOrder) ?>
+	</th>
+	<th width="5%" class="nowrap">
+		<?php echo JHtml::_('grid.sort', 'COM_ARVIE_PUBLICATIONS_PARENT', 'p.parent', $listDirn, $listOrder) ?>
+	</th>
+	<th width="2%" class="nowrap">
+		<?php echo JHtml::_('grid.sort', 'COM_ARVIE_PUBLICATIONS_GROUPE', 'p.groupe', $listDirn, $listOrder) ?>
+	</th>
+	<th width="5%" class="nowrap">
+		<?php echo JHtml::_('grid.sort', 'COM_ARVIE_PUBLICATIONS_AUTEUR', 'p.auteur', $listDirn, $listOrder) ?>
+	</th>
+	<th width="20%" class="nowrap">
+		<?php echo JHtml::_('grid.sort', 'COM_ARVIE_PUBLICATIONS_TEXTE', 'p.texte', $listDirn, $listOrder) ?>
+	</th>
+	<th width="5%" style="min-width:55px" class="nowrap center ">
+		<?php echo JHtml::_('grid.sort', 'Publié', 'p.published', $listDirn, $listOrder) ?>
+	</th>
+	<th width="5%" style="min-width:120px" class="nowrap center hidden-phone">
+		<?php echo JHtml::_('grid.sort', 'Date', 'p.modified', $listDirn, $listOrder) ?>
+	</th>
+	<th width="5%" class="center hidden-tablet hidden-phone">
+		<?php echo JHtml::_('grid.sort', 'JGLOBAL_HITS', 'p.hits', $listDirn, $listOrder); ?>
+	</th>
+</tr>
+

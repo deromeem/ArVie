e2adfd6d8523001e088ea77d8286bf86837b57c5
Allan FORSON <allan.forson@louis-armand.paris>
2017-12-07 17:54:59 +0100

    Ajout/Modifications sur métiers en général

--- administrator/components/com_arvie/models/messages.php
+++ administrator/components/com_arvie/models/messages.php
@@ -1,7 +1,6 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-<<<<<<< HEAD
 class ArvieModelMessages extends JModelList
 {
 	public function __construct($config = array())
@@ -138,52 +137,3 @@ class ArvieModelMessages extends JModelList
 		return $createds;
 	}	
 }
-=======
-class ArvieModelMessages extends JModelAdmin
-{
-	protected $_compo = 'com_arvie';
-	protected $_context = 'messages';
-	public $typeAlias = 'com_arvie.messages';
-	
-	// Surcharges des m�thodes de la classe m�re pour :
-	
-	// 1) d�finir la table de soutien � utiliser
-	public function getTable($type = 'Messages', $prefix = 'ArvieTable', $config = array()) 
-	{
-		return JTable::getInstance($type, $prefix, $config);
-	}
-
-	// 2) pr�ciser le chemin du contexte � utiliser pour charger le fichier XML d�crivant les champs de saisie
-	public function getForm($data = array(), $loadData = true) 
-	{
-		$form = $this->loadForm($this->typeAlias, $this->_context,
-			array('control'=>'jform', 'load_data'=>$loadData));
-		if (empty($form)) 
-		{
-			return false;
-		}
-		return $form;
-	}
-
-	// 3) contr�ler qu'un tableau de donn�es n'est pas d�j� initialis� dans la session
-	protected function loadFormData() 
-	{
-		$data = JFactory::getApplication()->getUserState($this->_compo.'.edit.'.$this->_context.'.data', array());
-		if (empty($data)) 
-		{
-			$data = $this->getItem();
-		}
-		return $data;
-	}
-
-	// 4) pr�parer les donn�es avant la sauvegarde en base de donn�es par l'objet JTable
-	protected function prepareTable($table)
-	{
-		$table->alias = JApplication::stringURLSafe($table->alias);
-		if (empty($table->alias))
-		{
-			$table->alias = JApplication::stringURLSafe($table->nom);
-		}
-	}
-}
->>>>>>> 0bf3845e736d9c0bac9a20277d5a8fc552c09648
